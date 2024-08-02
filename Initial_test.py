ca7b72d0bf9c7b710f281af0921a43e8b8426433
Aqueouswater <benjtoa@gmail.com>
2024-08-01 23:27:50 -0700

    Lesson 4

--- Initial_test.py
+++ Initial_test.py
@@ -1,2 +1,17 @@
-## Comments
+## Video 04 - Subroutines
 
+from machine import Pin
+from time import sleep
+
+LED = Pin(16, Pin.OUT)
+LED2 = Pin(25, Pin.OUT)
+
+
+
+for i in range(0, 10):
+    LED.toggle()
+    LED2.toggle()
+    sleep(0.5)
+
+LED.value(0)
+LED2.value(0)
